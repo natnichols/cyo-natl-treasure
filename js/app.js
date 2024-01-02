66b001536d144bdc7b470a35bb52c51d1134bc29
NatNichols <natalie.e.nichols@gmail.com>
2024-01-02 15:58:13 -0500

    update flexbox/wireframe with descriptive placeholders for image, prompt, and buttons

--- js/app.js
+++ js/app.js
@@ -17,3 +17,25 @@
 /*------- Functions -------*/
 
 console.log(`Mulder, we're gonna steal the Declaration of Independence`)
+
+/*------- example from Ben for state variable (object) (or was it a constant to be put in a data file?):
+
+let story = [
+  {
+    promptNumber: 1,
+    imageToDisplay: '..assets/images/whatever.png',
+    prompt: 'You enter a room and see a door.',
+    options: [
+      {
+        option1: 'Open the door'
+        goTo: 3
+      }
+      {
+        option2: 'Examine the room you're currently in'
+        goTo: 2
+      }
+    ]
+  }
+]
+(was a render function somewhere in there??)
+-------*/
